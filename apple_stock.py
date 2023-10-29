6eced5fa51259f02b3f91d187c78ce02ddb845f7
Tech Kaya <techkayacodes@gmail.com>
2023-10-29 14:13:03 -0400

    modified python scraper scripts

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_apple_stock_data():
+    # Define the URL
+    url = 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL'
+    
+    # Define headers to mimic a browser request
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    
+    # Make a request to the website with headers
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    
+    # Parse the content using BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Find the table with historical data
+    table = soup.find('table', class_='W(100%) M(0)')
+    
+    # Check if table is found
+    if not table:
+        print("Unable to find the historical data table.")
+        return
+    
+    # Extract the rows from the table
+    rows = table.findAll('tr')
+    
+    # Iterate over the rows and extract the date and close price
+    for row in rows[1:]:  # Skip the header row
+        columns = row.findAll('td')
+        if len(columns) > 4:  # Ensure there are enough columns to extract data
+            date = columns[0].text
+            close_price = columns[4].text
+            print(f'Date: {date}, Close Price: {close_price}')
+
 if __name__ == "__main__":
-    pass
+    get_apple_stock_data()
--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_top_20_players():
+    # URL to the CBS NFL Stats webpage
+    url = "http://www.cbssports.com/nfl/stats"
+    
+    # Make a request to the webpage
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    
+    # Parse the webpage using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Extract the top 20 players based on the provided tags
+    players = soup.find_all('div', class_='StatsLeadersCard-featuredStat', limit=20)
+    
+    for player in players:
+        touchdowns = player.find('div', class_='StatsLeadersCard-statValue').text.strip()
+        stat_title = player.find('div', class_='StatsLeadersCard-statTitle').text.strip()
+        
+        # Assuming there are other tags for player's name, position, and team, you can extract them similarly
+        # For this example, I'll only print the touchdowns and stat title
+        print(f"{stat_title}: {touchdowns}")
+
 if __name__ == "__main__":
-    pass
+    get_top_20_players()
